79bd82c14e44987fb5dcc045260147040efe19d3
Milan Krunic <milankrunic12@gmail.com>
2021-09-23 14:44:12 +0200

    First commit

---
+++ app/src/main/java/com/example/messenger/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Initialisation of views
+        var userEmail: TextView = findViewById(R.id.et_userEmail)
+        var userPassword: TextView = findViewById(R.id.et_userPassword)
+        var logInButton : Button = findViewById(R.id.btn_login)
+
+        logInButton.setOnClickListener {
+
+        }
+
+
+
+    }
+}
